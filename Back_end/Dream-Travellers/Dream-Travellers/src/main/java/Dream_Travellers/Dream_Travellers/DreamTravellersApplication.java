f6887e9443c2848dddcce088fb0a2ed88497872d
Vutlhari23 <164008799+Vutlhari23@users.noreply.github.com>
2025-04-28 17:00:47 +0200

    Add files via upload

---
+++ Back_end/Dream-Travellers/Dream-Travellers/src/main/java/Dream_Travellers/Dream_Travellers/DreamTravellersApplication.java
@@ -0,0 +1,13 @@
+package Dream_Travellers.Dream_Travellers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamTravellersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamTravellersApplication.class, args);
+	}
+
+}
---
+++ Back_end/Dream-Travellers/Dream-Travellers/src/test/java/Dream_Travellers/Dream_Travellers/DreamTravellersApplicationTests.java
@@ -0,0 +1,13 @@
+package Dream_Travellers.Dream_Travellers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamTravellersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
